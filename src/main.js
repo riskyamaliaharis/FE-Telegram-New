66d9d73446e4a698b8712d5ac5e125a1196bfdd4
Bagusth15 <bagustri15@gmail.com>
2021-01-20 13:03:31 +0700

    add maps

--- src/main.js
+++ src/main.js
@@ -5,11 +5,21 @@ import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import * as VueGoogleMaps from "vue2-google-maps";
+import VueGeolocation from "vue-browser-geolocation";
+
+Vue.use(VueGeolocation);
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCNBqVrRQSZ93YtY0WmoXMG3T75SKIxE00",
+    libraries: "places" // This is required
+  }
+});
 
 new Vue({
   router,
--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="maps">
     <h1>Page Maps</h1>
+    <GmapMap
+      :center="coordinate"
+      :zoom="10"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker
+        :position="coordinate"
+        :clickable="true"
+        :draggable="true"
+        @click="clickMarker"
+        icon="https://img.icons8.com/color/48/000000/map-pin.png"
+      />
+    </GmapMap>
+    <h3>{{ coordinate }}</h3>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Maps",
+  data() {
+    return {
+      coordinate: {
+        lat: 10,
+        lng: 10
+      }
+    };
+  },
+  created() {
+    this.$getLocation()
+      .then(coordinates => {
+        this.coordinate = {
+          lat: coordinates.lat,
+          lng: coordinates.lng
+        };
+        console.log(coordinates);
+      })
+      .catch(error => {
+        alert(error);
+      });
+  },
+  methods: {
+    clickMarker(position) {
+      console.log("clicked marker");
+      console.log(position);
+      console.log(position.latLng.lat());
+      console.log(position.latLng.lng());
+      this.coordinate = {
+        lat: position.latLng.lat(),
+        lng: position.latLng.lng()
+      };
+    }
+  }
+};
+</script>
